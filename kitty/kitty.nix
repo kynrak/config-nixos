5255a6b96b2e050425f8728c78d7496e3732fdf8
Luke Teo <yipengteo.luke@gmail.com>
2024-12-18 19:22:12 +0900

    fix: kitty fonts and tab style

--- kitty/kitty.nix
+++ kitty/kitty.nix
@@ -1,8 +1,8 @@
 {
   programs.kitty = {
     font = { 
-      name = "JetBrainsMono Nerd Font Mono";
-      size = 12;
+      name = "BigBlueTermPlus Nerd Font";
+      size = 10;
     };
     settings = { 
       background_opacity = "1"; 
@@ -12,6 +12,9 @@
       vi_mode_cursor_shape = "block"; 
       scrollback_always_clear = "no"; 
       scroll_wheel_multiplier = 5;
+      tab_bar_edge = "top"; 
+      tab_bar_style = "slant"; 
+      tab_title_template = "{index} {title}";
     };
     keybindings = { 
       "ctrl+c" = "copy_to_clipboard"; 
