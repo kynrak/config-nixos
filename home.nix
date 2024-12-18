3192170a127180e1056cf36d0fd49c38069465f7
Luke Teo <yipengteo.luke@gmail.com>
2024-12-18 19:44:29 +0900

    feat: added pks for dev unix, and networking

--- home.nix
+++ home.nix
@@ -26,23 +26,25 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+  home.packages = with pkgs; [
+    # Dev Tools 
+    go 
+    go-task 
+    nodejs
+    bun
+    python3
 
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+    # Unix Tools 
+    ncdu 
+    ripgrep 
+    fd 
+    tree 
+    fzf 
+    eza 
+    bat 
 
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+    # Network 
+    nmap 
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
