cc4acaa2b54a1f1ee9dfebd3944b143607ad7c8a
luke-teo <67887422+lukteo@users.noreply.github.com>
2024-12-24 22:58:58 +0900

    fix: vimnix lsp keymaps

--- nvim/plugins.nix
+++ nvim/plugins.nix
@@ -23,6 +23,69 @@
         ruff.enable = true;
         nixd.enable = true;
       };
+      keymaps = {
+        diagnostic = {
+          "[d" = {
+            action = "goto_prev";
+            desc = "Go to prev diagnostic";
+          };
+          "]d" = {
+            action = "goto_next";
+            desc = "Go to next diagnostic";
+          };
+          "<leader>cd" = {
+            action = "open_float";
+            desc = "Show Line Diagnostics";
+          };
+        };
+
+        lspBuf = {
+          "<leader>ca" = {
+            action = "code_action";
+            desc = "Code Actions";
+          };
+          "<leader>rn" = {
+            action = "rename";
+            desc = "Rename Symbol";
+          };
+          "<leader>f" = {
+            action = "format";
+            desc = "Format";
+          };
+          "gd" = {
+            action = "definition";
+            desc = "Goto definition (assignment)";
+          };
+          "gD" = {
+            action = "declaration";
+            desc = "Goto declaration (first occurrence)";
+          };
+          "gy" = {
+            action = "type_definition";
+            desc = "Goto Type Defition";
+          };
+          "gi" = {
+            action = "implementation";
+            desc = "Goto Implementation";
+          };
+          "<leader>K" = {
+            action = "hover";
+            desc = "Hover";
+          };
+          "<leader>sh" = {
+            action = "signature_help";
+            desc = "Signature Help";
+          };
+          "<leader>gr" = {
+            action = "references";
+            desc = "References to thing";
+          };
+          "<leader>vws" = {
+            action = "workspace_symbol";
+            desc = "Workspace symbol";
+          };
+        };
+      };
     };
     conform-nvim = {
       enable = true;
